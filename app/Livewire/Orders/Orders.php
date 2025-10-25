61c3268e7a64ac1676bf2d51a3cb072aa75677e0
Jaap Goorhuis <info@crewa.nl>
2025-10-25 11:56:45 +0200

    order goedgemaakt en offerte

--- app/Livewire/Orders/Orders.php
+++ app/Livewire/Orders/Orders.php
@@ -98,8 +98,15 @@ class Orders extends Component
 
         Pdf::loadView('pdf.orderlijst',['order' => $order, 'leverancier'=> $leverancier])->save(public_path('/storage/orderlijst/order-'.$order->order_id.'.pdf'));
 
-        if($leverancier->suplier_email != '') {
-            Mail::to($leverancier->suplier_email)->send(new sendOrderList($order));
+        try {
+            if ($leverancier->suplier_email != '') {
+                Mail::to(strtolower($leverancier->suplier_email))->send(new sendOrderList($order));
+            } else {
+                session()->flash('error','Er is geen email beschikbaar van deze leverancier.');
+                return $this->redirect('/orders', navigate: true);
+            }
+        } catch (\Exception $e) {
+            return redirect('/orders')->with('error', 'Er is een fout opgetreden bij het versturen van de e-mail.');
         }
 
         Order::where('id', $order->id)->update([
