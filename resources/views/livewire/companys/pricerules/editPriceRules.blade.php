62eff5949fd420480fb7d3df4cf827bcf3f2bd7f
Jaap Goorhuis <info@crewa.nl>
2025-10-28 20:12:08 +0100

    fixed pricerules en dealers

--- resources/views/livewire/companys/pricerules/editPriceRules.blade.php
+++ resources/views/livewire/companys/pricerules/editPriceRules.blade.php
@@ -71,12 +71,12 @@
                         </div>
 
                         <div class="grid md:grid-cols-2 md:gap-6">
-                            @if($this->priceRule->company_id !== 0)
-                            <div class="relative z-0 w-full mb-5 group">
-                                <label for="rietpanel_panel_price" class="text-gray-400">Rietpanel's prijs per m² *</label>
-                                <input type="number" wire:model="rietpanel_panel_price" name="rietpanel_panel_price" id="rietpanel_panel_price" @if(Auth::user()->is_admin != 1) disabled @endif class="disabled:hover:cursor-not-allowed block py-2.5 px-0 w-full text-md text-gray-900 border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-b-[#C0A16E]" placeholder=" " required />
-                            </div>
-                            @endif
+{{--                            @if($this->priceRule->company_id !== 0)--}}
+{{--                            <div class="relative z-0 w-full mb-5 group">--}}
+{{--                                <label for="rietpanel_panel_price" class="text-gray-400">Rietpanel's prijs per m² *</label>--}}
+{{--                                <input type="number" wire:model="rietpanel_panel_price" name="rietpanel_panel_price" id="rietpanel_panel_price" @if(Auth::user()->is_admin != 1) disabled @endif class="disabled:hover:cursor-not-allowed block py-2.5 px-0 w-full text-md text-gray-900 border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-b-[#C0A16E]" placeholder=" " required />--}}
+{{--                            </div>--}}
+{{--                            @endif--}}
 
                             <div class="relative z-0 w-full mb-5 group">
                                 <label for="panel_price" class="text-gray-400">Prijs per m² *</label>
