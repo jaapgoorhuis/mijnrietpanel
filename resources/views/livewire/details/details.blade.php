7d7b2abb308867596a5a121fcf8e7b9e092a92f3
Jaap Goorhuis <info@crewa.nl>
2026-01-12 12:53:33 +0100

    lijst 6-1-2026 aangepast

--- resources/views/livewire/details/details.blade.php
+++ resources/views/livewire/details/details.blade.php
@@ -21,7 +21,7 @@
 
     <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
+            <div class="p-6 text-gray-900 relative">
                 @admin
                 <div class="text-right">
                     <button wire:click="uploadDetails()" type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
@@ -36,7 +36,7 @@
                         wire:loading.attr="disabled"
                         wire:target="downloadAll"
                         type="button"
-                        class="disabled:cursor-not-allowed text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                        class=" absolute top-[25px] disabled:cursor-not-allowed text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                     >
                         <!-- Spinner verschijnt alleen tijdens downloadAll() -->
                         <span wire:loading wire:target="downloadAll">
--- resources/views/livewire/pricelist/pricelist.blade.php
+++ resources/views/livewire/pricelist/pricelist.blade.php
@@ -39,7 +39,7 @@
 
         @endif
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
+            <div class="p-6 text-gray-900 relative">
                 @admin
                 <div class="text-right">
                     <button wire:click="uploadPricelist()" type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
@@ -55,7 +55,7 @@
                         wire:loading.attr="disabled"
                         wire:target="downloadAll"
                         type="button"
-                        class="disabled:cursor-not-allowed text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                        class="absolute top-[25px] disabled:cursor-not-allowed text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                     >
                         <!-- Spinner verschijnt alleen tijdens downloadAll() -->
                         <span wire:loading wire:target="downloadAll">
