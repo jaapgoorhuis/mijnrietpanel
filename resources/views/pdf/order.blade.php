3cd2defb9141eb655adc094cd243e1d9f8a04510
Jaap Goorhuis <info@crewa.nl>
2026-02-12 19:35:31 +0100

    test

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -193,11 +193,11 @@
                 <tr class="items">
                     <td>{{$order->comment}}</td>
                     @if($order->status === 'Bevestigd')
-
+                        @if($order->orderRules)
                             <td>
                                 {!! '&euro;&nbsp;' . number_format($order->orderRules->price, 2, ',', '.') !!}
                             </td>
-
+                        @endif
                     @endif
                 </tr>
             </table>
