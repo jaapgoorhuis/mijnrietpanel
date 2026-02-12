bd9e0ed49cc56bfd5675fa82543f0f65d2ba92a8
Jaap Goorhuis <info@crewa.nl>
2026-02-12 19:33:42 +0100

    test

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -190,16 +190,17 @@
                 <tr class="items">
                     <td><strong>{{ __('messages.Klant opmerking') }}</strong></td>
                 </tr>
-{{--                <tr class="items">--}}
-{{--                    <td>{{$order->comment}}</td>--}}
-{{--                    @if($order->status == 'Bevestigd')--}}
-{{--                        @if($order->orderRules)--}}
-{{--                            <td>--}}
-{{--                                {!! '&euro;&nbsp;' . number_format($order->orderRules->price, 2, ',', '.') !!}--}}
-{{--                            </td>--}}
-{{--                        @endif--}}
-{{--                    @endif--}}
-{{--                </tr>--}}
+                <tr class="items">
+                    <td>{{$order->comment}}</td>
+                    @if($order->status == 'Bevestigd')
+                        test
+                        @if($order->orderRules)
+                            <td>
+                                {!! '&euro;&nbsp;' . number_format($order->orderRules->price, 2, ',', '.') !!}
+                            </td>
+                        @endif
+                    @endif
+                </tr>
             </table>
 
         @endif
