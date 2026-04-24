eda2b6b844a4f729d4305d52b4395b6797236683
Jaap Goorhuis <info@crewa.nl>
2026-04-24 19:03:41 +0200

    tesT

--- resources/views/livewire/marketingFolder/marketing/marketing.blade.php
+++ resources/views/livewire/marketingFolder/marketing/marketing.blade.php
@@ -30,7 +30,6 @@
     <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 relative">
-                @admin
                 <div class="flex flex-col gap-3 md:flex-row md:justify-between">
 
                     @admin
